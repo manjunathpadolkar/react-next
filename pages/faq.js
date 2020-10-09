156c13cb4081092e2a61cd750cf51b5715102f40
Manjunath Padolkar <manjunathrpadolkar@gmail.com>
2020-10-09 10:25:44 -0400

    addmin banner image

--- pages/faq.js
+++ pages/faq.js
@@ -12,7 +12,7 @@ function Faq() {
               <link rel="icon" href="/favicon.ico" />
           </Head>
           <div className={styles.main}>
-            <Typography variant="h3" gutterBottom>FAQ</Typography>
+            <Typography variant="h1" gutterBottom>FAQ</Typography>
             <div>
               <p>Hero can be anyone. Even a man knowing something as simple and reassuring as putting a coat around a young boy shoulders to let him know the world hadn’t ended. You fight like a younger man, there’s nothing held back. It’s admirable, but mistaken. When their enemies were at the gates the Romans would suspend democracy and appoint one man to protect the city. It wasn’t considered an honor, it was a public service.</p>
               <p>Hero can be anyone. Even a man knowing something as simple and reassuring as putting a coat around a young boy shoulders to let him know the world hadn’t ended. You fight like a younger man, there’s nothing held back. It’s admirable, but mistaken. When their enemies were at the gates the Romans would suspend democracy and appoint one man to protect the city. It wasn’t considered an honor, it was a public service.</p>
